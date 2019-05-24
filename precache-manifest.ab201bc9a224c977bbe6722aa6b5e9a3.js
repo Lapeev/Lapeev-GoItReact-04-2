769b9a164f5c48a43bf9271f5c68a440793e333b
lapeev16@gmail.com <lapeev16@gmail.com>
2019-05-24 16:37:46 +0300

    Updates

---
+++ precache-manifest.ab201bc9a224c977bbe6722aa6b5e9a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1059e0d661d7ec6c65d0",
+    "url": "/Lapeev-GoItReact-04-2/static/js/runtime~main.404be5dc.js"
+  },
+  {
+    "revision": "2d1ca351a9063cb7c6b3",
+    "url": "/Lapeev-GoItReact-04-2/static/js/main.31b6396a.chunk.js"
+  },
+  {
+    "revision": "c9c1651401add314094d",
+    "url": "/Lapeev-GoItReact-04-2/static/js/3.f7c9f339.chunk.js"
+  },
+  {
+    "revision": "fb2e1f8273e532712e70",
+    "url": "/Lapeev-GoItReact-04-2/static/js/2.617de1a3.chunk.js"
+  },
+  {
+    "revision": "2d1ca351a9063cb7c6b3",
+    "url": "/Lapeev-GoItReact-04-2/static/css/main.497064b0.chunk.css"
+  },
+  {
+    "revision": "c9c1651401add314094d",
+    "url": "/Lapeev-GoItReact-04-2/static/css/3.2e967e6d.chunk.css"
+  },
+  {
+    "revision": "fb2e1f8273e532712e70",
+    "url": "/Lapeev-GoItReact-04-2/static/css/2.ece6308d.chunk.css"
+  },
+  {
+    "revision": "ab9f564d4592e869d6e5c03073c3bfdb",
+    "url": "/Lapeev-GoItReact-04-2/index.html"
+  }
+];
